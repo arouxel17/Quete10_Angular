37ebb25928bbb034e70c7d3a47ebad65e3adee11
arouxel17 <alexandrerouxel17@gmail.com>
2023-02-24 11:26:13 +0100

    quete 11 angular

--- src/app/service/cocktail-service.service.ts
+++ src/app/service/cocktail-service.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { Cocktail } from '../models/cocktail';
 
 @Injectable({
@@ -6,16 +8,13 @@ import { Cocktail } from '../models/cocktail';
 })
 export class CocktailService {
 
-  constructor() { }
+  private readonly cocktailApi = "assets/cocktails.json";
+  constructor(public http: HttpClient) { }
 
-  getCocktails(): Array<Cocktail>{
-    let cocktails = [
-      new Cocktail('Bloody Mary', 9, 'https://www.cocktailmag.fr/media/k2/items/cache/1c0ae2205709722b62e843abc0471a55_S.jpg'),
-      new Cocktail('Tequila Sunrise', 10, 'https://www.cocktailmag.fr/media/k2/items/cache/2fa67f482133f1c934235b73c2a03954_S.jpg'),
-      new Cocktail('Salty Dog', 8, 'https://www.cocktailmag.fr/media/k2/items/cache/0b1ad7a7b79268a1f4558db78e092446_S.jpg'),
-      new Cocktail('Balalaïka', 11, 'https://www.cocktailmag.fr/media/k2/items/cache/ccb4e23c8aa216f1e96d31ab209c036b_S.jpg'),
-    ];
-
-    return cocktails;
+  public getCocktails(): Observable<Cocktail[]> {
+    return this.http.get<Cocktail[]>(this.cocktailApi).pipe(
+      tap(cocktail=>console.log('cocktails:', cocktail))
+    );
   }
+
 }
